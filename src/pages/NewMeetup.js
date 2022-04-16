b37c43da479d7a38b452f3d5c565bb833bcaa3b5
Sherif Abdellatif <sharif_pc@hotmail.com>
2022-04-16 16:41:40 +0200

    End Of React Crash Course

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,27 @@
+import { useHistory } from 'react-router-dom';
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://dogwood-abacus-257510-default-rtdb.europe-west1.firebasedatabase.app/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    ).then(() => {
+      history.replace('/');
+    });
+  }
   return (
     <section>
-      <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <h1>Add New Meetup </h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
